36495f39a86c8060f17107ac6d800214574b8075
Konrad Zielinski <kzielinski@pivotal.io>
2015-11-30 20:46:50 +0000

    TDD for recipe model, spec and validations

---
+++ test/models/recipe_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+  
+  def setup
+    @recipe = Recipe.new(name: "test name", summary: "test summary", description: "test description long")
+  end
+  
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+  
+  test "name should be present" do
+    @recipe.name = ""
+    assert_not @recipe.valid?
+  end
+  
+  test "name length should not be too long" do
+    @recipe.name = "a" * 101
+    assert_not @recipe.valid?
+  end
+  
+  test "name length should not be too short" do
+    @recipe.name = "abcd"
+    assert_not @recipe.valid?
+  end
+  
+  test "summary should be present" do
+    @recipe.summary = ""
+    assert_not @recipe.valid?
+  end
+  
+  test "summary length should not be too long" do
+    @recipe.summary = "a" * 151
+    assert_not @recipe.valid?
+  end
+  
+  test "summary length should not be too short" do
+    @recipe.summary = "a" * 9
+    assert_not @recipe.valid?
+  end
+  
+  test "description should be present" do
+    @recipe.description = ""
+    assert_not @recipe.valid?
+  end
+  
+  test "description length should not be too long" do
+    @recipe.description = "a" * 501
+    assert_not @recipe.valid?
+  end
+  
+  test "description length should not be too short" do
+    @recipe.description = "a" * 19
+    assert_not @recipe.valid?
+  end
+  
+end
