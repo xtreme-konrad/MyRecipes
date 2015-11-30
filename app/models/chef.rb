9ef1d6172f89844c29e9d2580b06c7c7021ca842
Konrad Zielinski <kzielinski@pivotal.io>
2015-11-30 21:40:31 +0000

    one to many association between chef and recipes

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,8 @@
 class Chef < ActiveRecord::Base
+  has_many :recipes
+  
+  before_save { self.email = email.downcase }
+  
   validates :chefname, presence: true, 
                        length: { minimum: 3, maximum: 40 }
                        
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,11 +3,17 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
   
   def setup
-    @recipe = Recipe.new(name: "test name", summary: "test summary", description: "test description long")
+    @chef = Chef.create(chefname:"test name", email:"test@example.com")
+    @recipe = @chef.recipes.build(name: "test name", summary: "test summary", description: "test description long")
   end
   
   test "recipe should be valid" do
     assert @recipe.valid?
+  end
+  
+  test "chef_id should be present" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
   end
   
   test "name should be present" do
