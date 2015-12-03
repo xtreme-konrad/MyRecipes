f874de505555504644acefdb850068e009de2100
Konrad Zielinski <kzielinski@pivotal.io>
2015-12-03 20:14:55 +0000

    add pagination to views

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -13,6 +13,8 @@ class Recipe < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   validate :picture_size
   
+  default_scope -> { order(updated_at: :desc) }
+  
   def thumbs_up_total
     self.likes.where(like: true).size
   end
